9e4f062d3c1f6b9c6ea1c9bebbd94a7769209712
FumiyukiKato <fumilemon79@gmail.com>
2022-04-02 18:41:50 +0900

    :recycle: remove print debug

--- gatsby/on-post-build.js
+++ gatsby/on-post-build.js
@@ -1,32 +1,17 @@
 'use strict';
 
 const onPostBuild = () => {
-    console.log('#############################')
-    console.log(process.cwd())
     const glob = require('glob')
     const cheerio = require('cheerio')
     const fs = require('fs')
-    // files = glob.sync(`/`)
-    // console.log(files)
-    // files.forEach((file) => 
-    //     console.log(file)
-    // )
-    // files = glob.sync(`.`)
-    // console.log(files)
-    // files.forEach((file) => 
-    //     console.log(file)
-    // )
 
     const postFiles = glob.sync(`public/posts/**/*.html`)
     postFiles.forEach((file) => {
-        console.log(file)
         const $ = cheerio.load(fs.readFileSync(file).toString());
         const pForKatex = $('<p></p>');
-        console.log($('.katex-display'))
         $('.katex-display').wrap(pForKatex);
         fs.writeFileSync(file, $.html())
     })
-    console.log('#############################')
 }
 
 module.exports = onPostBuild
