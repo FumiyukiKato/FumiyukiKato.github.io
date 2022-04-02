d63af8c96bec87b43feb79fcb2d3f3a05584d961
FumiyukiKato <fumilemon79@gmail.com>
2022-04-02 16:48:38 +0900

    :bug: fix hook

--- gatsby/on-post-build.js
+++ gatsby/on-post-build.js
@@ -4,7 +4,7 @@ exports.onPostBuild = () => {
     const postFiles = glob(`public/posts/*/index.html`);
     const cheerio = require('cheerio')
     const pForKatex = $('<p></p>');
-    htmlFiles.forEach((file) => {
+    postFiles.forEach((file) => {
        const $ = cheerio.load(fs.readFileSync(file).toString());
        $('.katex-display').wrap(pForKatex);
        fs.writeFileSync(file, $.html())
