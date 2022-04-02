df75d357fb1772bda023b07e7a611ed83108b17d
FumiyukiKato <fumilemon79@gmail.com>
2022-04-02 18:39:11 +0900

    :bug: use onPostBuild hooks to rewrite html

--- gatsby/on-post-build.js
+++ gatsby/on-post-build.js
@@ -1,12 +1,32 @@
 'use strict';
 
-exports.onPostBuild = () => {
-    const postFiles = glob(`public/posts/*/index.html`);
+const onPostBuild = () => {
+    console.log('#############################')
+    console.log(process.cwd())
+    const glob = require('glob')
     const cheerio = require('cheerio')
-    const pForKatex = $('<p></p>');
+    const fs = require('fs')
+    // files = glob.sync(`/`)
+    // console.log(files)
+    // files.forEach((file) => 
+    //     console.log(file)
+    // )
+    // files = glob.sync(`.`)
+    // console.log(files)
+    // files.forEach((file) => 
+    //     console.log(file)
+    // )
+
+    const postFiles = glob.sync(`public/posts/**/*.html`)
     postFiles.forEach((file) => {
-       const $ = cheerio.load(fs.readFileSync(file).toString());
-       $('.katex-display').wrap(pForKatex);
-       fs.writeFileSync(file, $.html())
+        console.log(file)
+        const $ = cheerio.load(fs.readFileSync(file).toString());
+        const pForKatex = $('<p></p>');
+        console.log($('.katex-display'))
+        $('.katex-display').wrap(pForKatex);
+        fs.writeFileSync(file, $.html())
     })
+    console.log('#############################')
 }
+
+module.exports = onPostBuild
