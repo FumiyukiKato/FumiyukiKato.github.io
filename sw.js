176d03cd843dec9b27878340ce360719aea445d9
FumiyukiKato <FumiyukiKato@users.noreply.github.com>
2022-06-08 05:14:44 +0000

    deploy: 44310f85f3e7917e37cc5763b2781f08bb85dcae

--- sw.js
+++ sw.js
@@ -27,35 +27,35 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-44aa0c327c6ab5494354.js"
+    "url": "webpack-runtime-6c160e38843e118d00d8.js"
   },
   {
-    "url": "styles.10a7c215c92c5fad0a53.css"
+    "url": "styles.c291d54da782f2b5e68f.css"
   },
   {
     "url": "styles-407fe62976dc5310c43e.js"
   },
   {
-    "url": "framework-1e331b2e9d55ebef6dec.js"
+    "url": "framework-e137f9478210b28fb59b.js"
   },
   {
-    "url": "532a2f07-549094fa347c85980056.js"
+    "url": "532a2f07-b32c28cb7cdd6cc07e70.js"
   },
   {
-    "url": "dc6a8720040df98778fe970bf6c000a41750d3ae-ae9f57b3df6a2a1f7657.js"
+    "url": "dc6a8720040df98778fe970bf6c000a41750d3ae-c96c3eb8ccd5ca3f0920.js"
   },
   {
-    "url": "app-27db8ed909eea169fe4b.js"
+    "url": "app-f8b1193d3b5108d6c8c1.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "a0de34aa96d6e73e35845d8165f5169e"
+    "revision": "b45975cfc839f9ce4c1510e7edca3026"
   },
   {
-    "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-fd4fb51a6fac1c18bdde.js"
+    "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-241020a1cf7d0f317bbe.js"
   },
   {
-    "url": "polyfill-8de6de3a6555a9e76093.js"
+    "url": "polyfill-bb8cb91566de2b9ecffa.js"
   },
   {
     "url": "manifest.json",
@@ -155,7 +155,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-27db8ed909eea169fe4b.js`))) {
+  if (!resources || !(await caches.match(`/app-f8b1193d3b5108d6c8c1.js`))) {
     return await fetch(event.request)
   }
 
